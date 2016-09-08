b8e603c894c46d74b35d4a3c7d302b836c05a9b9
RnMonkey <pqixing@163.com>
2016-09-08 16:50:40 +0800

    Http中间层

--- dCommonLibrary/src/main/java/com/dachen/common/http2/impl/AsyncHttpClientImpl.java
+++ dCommonLibrary/src/main/java/com/dachen/common/http2/impl/AsyncHttpClientImpl.java
@@ -2,6 +2,7 @@ package com.dachen.common.http2.impl;
 
 import android.content.Context;
 
+import com.dachen.common.http.HttpsSupportCompat;
 import com.dachen.common.http2.handle.AbsHandle;
 import com.dachen.common.http2.handle.ResponseHandle;
 import com.dachen.common.http2.request.AbsRequst;
@@ -16,10 +17,19 @@ import java.util.Map;
  */
 public class AsyncHttpClientImpl implements AbsHttpClient {
 
+    private static AsyncHttpClient mRealClient;
     private Context context;
 
     public AsyncHttpClientImpl(Context context) {
         this.context = context;
+        if (mRealClient == null) {
+            synchronized (AsyncHttpClientImpl.class) {
+                if (mRealClient == null) {
+                    mRealClient = new AsyncHttpClient();
+                    new HttpsSupportCompat(this.context).compatAsyncHttp(mRealClient.getHttpClient());
+                }
+            }
+        }
     }
 
     @Override
@@ -36,11 +46,18 @@ public class AsyncHttpClientImpl implements AbsHttpClient {
                 params.put(entry.getKey(), entry.getValue());
             }
         }
-        new AsyncHttpClient().post(URL, params, new ResponseHandle(handle));
+        mRealClient.post(URL, params, new ResponseHandle(handle));
     }
 
     @Override
     public void get(String URL, AbsRequst request, AbsHandle handle) {
-
+        Map<String, String> map = request.toMap();
+        RequestParams params = new RequestParams();
+        if (map != null) {
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                params.put(entry.getKey(), entry.getValue());
+            }
+        }
+        mRealClient.get(URL, params, new ResponseHandle(handle));
     }
 }
