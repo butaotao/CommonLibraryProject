0b63b14086ec3ddc91058f947902ea5125957d5f
RnMonkey <pqixing@163.com>
2016-09-09 09:32:51 +0800

    修改请求管理器，包装使用请求，不允许直接使用请求

--- dCommonLibrary/src/main/java/com/dachen/common/http2/HttpRequestWrap.java
+++ dCommonLibrary/src/main/java/com/dachen/common/http2/HttpRequestWrap.java
@@ -1,14 +1,16 @@
 package com.dachen.common.http2;
 
 
+import com.dachen.common.http2.handle.AbsHandle;
 import com.dachen.common.http2.impl.AsyncHttpClientImpl;
 import com.dachen.common.http2.interfaces.AbsHttpClient;
+import com.dachen.common.http2.request.AbsRequst;
 
 /**
  * 网络请求管理类
  * Created by pqixi on 2016/9/8 0008.
  */
-public class HttpClientManager {
+public final class HttpRequestWrap {
 
     private static AbsHttpClient mClient;
 
@@ -16,7 +18,11 @@ public class HttpClientManager {
         mClient = client;
     }
 
-    public static AbsHttpClient getClient() {
+    private HttpRequestWrap() {
+
+    }
+
+    private static AbsHttpClient getClient() {
         if (mClient == null) {
             synchronized (AbsHttpClient.class) {
                 if (mClient == null)
@@ -24,5 +30,13 @@ public class HttpClientManager {
             }
         }
         return mClient;
+    }
+
+    public static void post(String URL, AbsRequst request, AbsHandle handle) {
+        getClient().post(URL, request, handle);
+    }
+
+    public static void get(String URL, AbsRequst request, AbsHandle handle) {
+        getClient().get(URL, request, handle);
     }
 }
