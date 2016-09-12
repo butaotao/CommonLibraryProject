e1b30520d07ed459a33b00448d6b06ed82d5a95e
zhouyuandong <zhouyd@dachentech.com.cn>
2016-09-12 13:45:37 +0800

    修改~

--- communitylibrary/src/main/java/com/dachen/community/Adapter/GridImgAdapter.java
+++ communitylibrary/src/main/java/com/dachen/community/Adapter/GridImgAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.dachen.community.R;
+import com.dachen.community.activity.PublishActivity;
 import com.dachen.community.model.PictureModel;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -20,13 +21,16 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 public class GridImgAdapter extends BaseAdapter<PictureModel> {
 
     private ViewHolder holder;
+    private PictureModel mBean;
+    private PublishActivity mActivity;
 
-    public GridImgAdapter(Context mContext) {
+    public GridImgAdapter(Context mContext,PublishActivity activity) {
         super(mContext);
+        mActivity = activity;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             holder = new ViewHolder();
             convertView = LayoutInflater.from(mContext).inflate(R.layout.item_picture_layout, null);
@@ -37,17 +41,39 @@ public class GridImgAdapter extends BaseAdapter<PictureModel> {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        String netUrl = dataSet.get(position).getNetImg();
-        String localUrl = dataSet.get(position).getLocalImg();
 
-        if(TextUtils.isEmpty(netUrl)){//显示本地图片
-            if(localUrl.equals("add")){
-                holder.mImageView.setImageResource(R.drawable.add_pic);
+        if(parent.getChildCount()==position){
+         //正常的用于界面展示的View对象
+            mBean = dataSet.get(position);
+            String netUrl = dataSet.get(position).getNetImg();
+            String localUrl = dataSet.get(position).getLocalImg();
+
+            if(dataSet.get(position).isShowDel()){
+                holder.img_del.setVisibility(View.VISIBLE);
             }else{
-                ImageLoader.getInstance().displayImage("file://" + localUrl, holder.mImageView);
+                holder.img_del.setVisibility(View.GONE);
             }
-        }else{//显示网络图片
-            ImageLoader.getInstance().displayImage(netUrl,holder.mImageView);
+
+            if(TextUtils.isEmpty(netUrl)){//显示本地图片
+                if(localUrl.equals("add")){
+                    holder.mImageView.setImageResource(R.drawable.list_add);
+                }else{
+                    ImageLoader.getInstance().displayImage("file://" + localUrl, holder.mImageView);
+                }
+            }else{//显示网络图片
+                ImageLoader.getInstance().displayImage(netUrl,holder.mImageView);
+            }
+
+            holder.img_del.setTag(position);
+            holder.img_del.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int index = (int)view.getTag();
+                    mActivity.delPic(index);
+                }
+            });
+        } else {
+         //临时的position=0
         }
         return convertView;
     }
